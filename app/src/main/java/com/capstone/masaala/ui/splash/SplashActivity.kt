20f9edf0bfc07a8ff032da8a7f0a3b2390a7c660
Lukham <undirectshots@gmail.com>
2023-05-30 05:56:28 +0700

    Added SplashScreen

--- app/src/main/java/com/capstone/masaala/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/capstone/masaala/ui/splash/SplashActivity.kt
@@ -1,12 +1,24 @@
 package com.capstone.masaala.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.capstone.masaala.R
+import com.capstone.masaala.ui.MainActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeOut)
     }
 }
