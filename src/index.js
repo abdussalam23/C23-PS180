f6a069ed0ec7cee09cbd3c2187ddb1ddfb4bad10
abdussalam23 <125942660+abdussalam23@users.noreply.github.com>
2023-06-02 12:57:41 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const multer = require('multer');
-const userRoute = require('./routes/userRoute');
 require('dotenv').config();
 
 const storage = multer.diskStorage({
@@ -21,13 +20,17 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/image', express.static('./upload'));
+app.get('/', (req, res) => {
+  res.send('test');
+})
 
-app.use(userRoute);
 app.post('/', upload.single('test'), (req, res) => {
   console.log(req.file);
   res.send(req.file);
 });
 
-app.listen(3000, () => {
-  console.log('Server running on PORT 3000');
-});
+const port = process.env.SERVER_PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`server running on ${port}`);
+})
