fb029b2a0951f392d00304d9f927aa1d52bcd552
abdussalam23 <125942660+abdussalam23@users.noreply.github.com>
2023-06-02 08:16:30 +0700

    Update userRoute.js

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const { getUserHandler, registerUserHandler, loginUserHandler } = require('../controller/UserController');
+
 
 const spiceRoutes = express.Router();
 
-spiceRoutes.get('/users', getUserHandler);
-spiceRoutes.post('/user/register', registerUserHandler);
-spiceRoutes.post('/user/login', loginUserHandler);
+
 
 module.exports = spiceRoutes;
